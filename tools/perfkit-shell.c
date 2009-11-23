4c8013851f49daa0a15b3b9917518c2549051ca6
Christian Hergert <chris@dronelabs.com>
2009-11-22 23:27:20 -0800

    [perfkit-shell] Implement `channel stop' command.

--- tools/perfkit-shell.c
+++ tools/perfkit-shell.c
@@ -849,6 +849,36 @@ channel_stop_cb (EggLine  *line,
                   gchar  **argv,
                   GError **error)
 {
+	DBusGProxy *channel;
+	gint        id = 0, i;
+	gchar      *path;
+
+	if (argc < 1)
+		return EGG_LINE_STATUS_BAD_ARGS;
+
+	for (i = 0; i < argc; i++) {
+		if (parse_int (argv [i], &id)) {
+			path = g_strdup_printf (PK_CHANNEL_FORMAT, id);
+			channel = dbus_g_proxy_new_for_name (dbus_conn,
+			                                     "com.dronelabs.Perfkit",
+			                                     path,
+			                                     "com.dronelabs.Perfkit.Channel");
+			if (!com_dronelabs_Perfkit_Channel_stop (channel, error)) {
+				REPORT_ERROR (*error);
+				g_error_free (*error);
+				*error = NULL; /* XXX: Propagate? */
+			}
+			else {
+				g_print ("Channel %d stopped.\n", id);
+			}
+			g_object_unref (channel);
+			g_free (path);
+		}
+		else {
+			g_printerr ("Invalid channel \"%s\".\n", argv [i]);
+		}
+	}
+
 	return EGG_LINE_STATUS_OK;
 }
 
