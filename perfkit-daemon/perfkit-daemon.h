63c217d82ff15b7202d2d806c6bd09570dc3a1ea
Christian Hergert <chris@dronelabs.com>
2009-11-27 15:28:27 -0800

    [daemon] Add master header file for plugins to include.

---
+++ perfkit-daemon/perfkit-daemon.h
@@ -0,0 +1,27 @@
+/* pkd.h
+ * 
+ * Copyright (C) 2009 Christian Hergert
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "pkd-channel.h"
+#include "pkd-channels.h"
+#include "pkd-log.h"
+#include "pkd-paths.h"
+#include "pkd-runtime.h"
+#include "pkd-sample.h"
+#include "pkd-source.h"
+#include "pkd-sources.h"
+#include "pkd-version.h"
