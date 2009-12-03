29e7fbcd45b17a2441357d8b4efdf7607e1a79b5
Christian Hergert <chris@dronelabs.com>
2009-12-02 23:16:32 -0800

    [libperfkit] Source cleanup of pk-connection-dbus.c.

--- perfkit/pk-connection-dbus.c
+++ perfkit/pk-connection-dbus.c
@@ -32,37 +32,6 @@
 
 G_DEFINE_TYPE (PkConnectionDBus, pk_connection_dbus, PK_TYPE_CONNECTION)
 
-static void     pk_connection_dbus_real_connect_async     (PkConnection         *connection,
-                                                           GAsyncReadyCallback   callback,
-                                                           gpointer              user_data);
-static gboolean pk_connection_dbus_real_connect_finish    (PkConnection         *connection,
-                                                           GAsyncResult         *result,
-                                                           GError              **error);
-static gboolean pk_connection_dbus_real_connect           (PkConnection         *connection,
-                                                           GError              **error);
-static void     pk_connection_dbus_real_disconnect        (PkConnection         *connection);
-static void     pk_connection_dbus_real_disconnect_async  (PkConnection         *connection,
-                                                           GAsyncReadyCallback   callback,
-                                                           gpointer              user_data);
-static void     pk_connection_dbus_real_disconnect_finish (PkConnection         *connection,
-                                                           GAsyncResult         *result);
-static gboolean pk_connection_dbus_real_channels_find_all (PkConnection         *connection,
-                                                           gint                **channel_ids,
-                                                           gint                 *n_channels);
-static gchar*   pk_connection_dbus_real_channel_get_target(PkConnection         *connection,
-                                                           gint                  channel_id);
-static gchar**  pk_connection_dbus_real_channel_get_args  (PkConnection         *connection,
-                                                           gint                  channel_id);
-static gchar*   pk_connection_dbus_real_channel_get_dir   (PkConnection         *connection,
-                                                           gint                  channel_id);
-static gchar**  pk_connection_dbus_real_channel_get_env   (PkConnection         *connection,
-                                                           gint                  channel_id);
-static GPid     pk_connection_dbus_real_channel_get_pid   (PkConnection         *connection,
-                                                           gint                  channel_id);
-static PkChannelState
-                pk_connection_dbus_real_channel_get_state (PkConnection         *connection,
-                                                           gint                  channel_id);
-
 struct _PkConnectionDBusPrivate
 {
 	GStaticRWLock    rw_lock;
@@ -70,53 +39,6 @@ struct _PkConnectionDBusPrivate
 	DBusGProxy      *channels;
 	DBusGProxy      *sources;
 };
-
-static void
-pk_connection_dbus_finalize (GObject *object)
-{
-	G_OBJECT_CLASS (pk_connection_dbus_parent_class)->finalize (object);
-}
-
-static void
-pk_connection_dbus_class_init (PkConnectionDBusClass *klass)
-{
-	GObjectClass      *object_class;
-	PkConnectionClass *conn_class;
-
-	object_class = G_OBJECT_CLASS (klass);
-	object_class->finalize = pk_connection_dbus_finalize;
-	g_type_class_add_private (object_class, sizeof (PkConnectionDBusPrivate));
-
-	conn_class = PK_CONNECTION_CLASS (klass);
-	conn_class->connect = pk_connection_dbus_real_connect;
-	conn_class->connect_finish = pk_connection_dbus_real_connect_finish;
-	conn_class->connect_finish = pk_connection_dbus_real_connect_finish;
-	conn_class->disconnect = pk_connection_dbus_real_disconnect;
-	conn_class->disconnect_async = pk_connection_dbus_real_disconnect_async;
-	conn_class->disconnect_finish = pk_connection_dbus_real_disconnect_finish;
-	conn_class->channels_find_all = pk_connection_dbus_real_channels_find_all;
-	conn_class->channel_get_target = pk_connection_dbus_real_channel_get_target;
-	conn_class->channel_get_args = pk_connection_dbus_real_channel_get_args;
-	conn_class->channel_get_dir = pk_connection_dbus_real_channel_get_dir;
-	conn_class->channel_get_env = pk_connection_dbus_real_channel_get_env;
-	conn_class->channel_get_pid = pk_connection_dbus_real_channel_get_pid;
-	conn_class->channel_get_state = pk_connection_dbus_real_channel_get_state;
-}
-
-static void
-pk_connection_dbus_init (PkConnectionDBus *connection)
-{
-	connection->priv = G_TYPE_INSTANCE_GET_PRIVATE (connection,
-	                                                PK_TYPE_CONNECTION_DBUS,
-	                                                PkConnectionDBusPrivate);
-	g_static_rw_lock_init (&connection->priv->rw_lock);
-}
-
-PkConnection*
-pk_connection_dbus_new (void)
-{
-	return g_object_new (PK_TYPE_CONNECTION_DBUS, NULL);
-}
 
 static DBusGProxy*
 pk_channel_proxy_new (PkConnection *connection,
@@ -137,14 +59,19 @@ pk_channel_proxy_new (PkConnection *connection,
 
 	return proxy;
 }
-                      
+
+static gboolean
+pk_connection_is_connected_locked (PkConnection *connection)
+{
+	return (PK_CONNECTION_DBUS (connection)->priv->dbus != NULL);
+}
 
 static gboolean
 pk_connection_dbus_real_connect (PkConnection  *connection,
                                  GError       **error)
 {
 	PkConnectionDBusPrivate *priv;
-	gboolean                 success;
+	gboolean                 success = FALSE;
 
 	g_return_val_if_fail (PK_IS_CONNECTION_DBUS (connection), FALSE);
 
@@ -152,27 +79,29 @@ pk_connection_dbus_real_connect (PkConnection  *connection,
 
 	g_static_rw_lock_writer_lock (&priv->rw_lock);
 
-	if (priv->dbus) {
+	if (pk_connection_is_connected_locked (connection)) {
 		g_set_error (error, PK_CONNECTION_ERROR,
 		             PK_CONNECTION_ERROR_INVALID,
 		             _("The connection is already connected"));
-		success = FALSE;
 	}
 	else {
 		/* TODO: Support multiple bus types */
-		priv->dbus = dbus_g_bus_get (DBUS_BUS_SESSION, error);
-		success = (priv->dbus != NULL);
-	}
-
-	if (success) {
-		priv->channels = dbus_g_proxy_new_for_name (priv->dbus,
-		                                            "com.dronelabs.Perfkit",
-		                                            "/com/dronelabs/Perfkit/Channels",
-		                                            "com.dronelabs.Perfkit.Channels");
-		priv->sources = dbus_g_proxy_new_for_name (priv->dbus,
-		                                           "com.dronelabs.Perfkit",
-		                                           "/com/dronelabs/Perfkit/Sources",
-		                                           "com.dronelabs.Perfkit.Sources");
+		if (NULL != (priv->dbus = dbus_g_bus_get (DBUS_BUS_SESSION, error))) {
+			priv->channels = dbus_g_proxy_new_for_name (priv->dbus,
+			                                            "com.dronelabs.Perfkit",
+			                                            "/com/dronelabs/Perfkit/Channels",
+			                                            "com.dronelabs.Perfkit.Channels");
+			priv->sources = dbus_g_proxy_new_for_name (priv->dbus,
+			                                           "com.dronelabs.Perfkit",
+			                                           "/com/dronelabs/Perfkit/Sources",
+			                                           "com.dronelabs.Perfkit.Sources");
+			success = TRUE;
+		}
+		else {
+			g_set_error (error, PK_CONNECTION_ERROR,
+			             PK_CONNECTION_ERROR_INVALID,
+			             _("Could not connect to DBUS"));
+		}
 	}
 
 	g_static_rw_lock_writer_unlock (&priv->rw_lock);
@@ -222,23 +151,27 @@ pk_connection_dbus_real_disconnect (PkConnection *connection)
 	priv = PK_CONNECTION_DBUS (connection)->priv;
 
 	g_static_rw_lock_writer_lock (&priv->rw_lock);
-	if (priv->dbus) {
+
+	if (pk_connection_is_connected_locked (connection)) {
 		dbus_g_connection_unref (priv->dbus);
 		priv->dbus = NULL;
+
 		if (priv->channels)
 			g_object_unref (priv->channels);
 		priv->channels = NULL;
+
 		if (priv->sources)
 			g_object_unref (priv->sources);
 		priv->sources = NULL;
 	}
+
 	g_static_rw_lock_writer_unlock (&priv->rw_lock);
 }
 
 static void
-pk_connection_dbus_real_disconnect_async  (PkConnection         *connection,
-                                           GAsyncReadyCallback   callback,
-                                           gpointer              user_data)
+pk_connection_dbus_real_disconnect_async (PkConnection        *connection,
+                                          GAsyncReadyCallback  callback,
+                                          gpointer             user_data)
 {
 	GSimpleAsyncResult *result;
 
@@ -261,6 +194,7 @@ pk_connection_dbus_real_disconnect_finish (PkConnection *connection,
 	g_return_if_fail (g_simple_async_result_is_valid (result,
 	                                                  G_OBJECT (connection),
 	                                                  pk_connection_dbus_real_disconnect_async));
+
 	pk_connection_dbus_real_disconnect (connection);
 }
 
@@ -272,16 +206,17 @@ pk_connection_dbus_real_channels_find_all (PkConnection  *connection,
 	PkConnectionDBusPrivate  *priv;
 	GPtrArray                *paths = NULL;
 	gboolean                  result;
-	gint                      i, id;
-	gchar                    *str;
+	gint                      i,
+	                          id;
+	const gchar              *str;
 
 	g_return_val_if_fail (channel_ids != NULL, FALSE);
 	g_return_val_if_fail (n_channels != NULL, FALSE);
 
 	priv = PK_CONNECTION_DBUS (connection)->priv;
 
-	if (!priv->channels) {
-		g_warning (_("Connection not open.  Open connection first."));
+	if (!pk_connection_is_connected_locked (connection)) {
+		g_warning (_("Connection not connected.  Please connect first."));
 		return FALSE;
 	}
 
@@ -301,6 +236,9 @@ pk_connection_dbus_real_channels_find_all (PkConnection  *connection,
 			}
 		}
 	}
+
+	if (paths)
+		g_ptr_array_unref (paths);
 
 	return result;
 }
@@ -417,4 +355,51 @@ pk_connection_dbus_real_channel_get_state (PkConnection *connection,
 	g_object_unref (proxy);
 
 	return state;
+}
+
+static void
+pk_connection_dbus_finalize (GObject *object)
+{
+	G_OBJECT_CLASS (pk_connection_dbus_parent_class)->finalize (object);
+}
+
+static void
+pk_connection_dbus_class_init (PkConnectionDBusClass *klass)
+{
+	GObjectClass      *object_class;
+	PkConnectionClass *conn_class;
+
+	object_class           = G_OBJECT_CLASS (klass);
+	object_class->finalize = pk_connection_dbus_finalize;
+	g_type_class_add_private (object_class, sizeof (PkConnectionDBusPrivate));
+
+	conn_class                     = PK_CONNECTION_CLASS (klass);
+	conn_class->connect            = pk_connection_dbus_real_connect;
+	conn_class->connect_finish     = pk_connection_dbus_real_connect_finish;
+	conn_class->connect_finish     = pk_connection_dbus_real_connect_finish;
+	conn_class->disconnect         = pk_connection_dbus_real_disconnect;
+	conn_class->disconnect_async   = pk_connection_dbus_real_disconnect_async;
+	conn_class->disconnect_finish  = pk_connection_dbus_real_disconnect_finish;
+	conn_class->channels_find_all  = pk_connection_dbus_real_channels_find_all;
+	conn_class->channel_get_target = pk_connection_dbus_real_channel_get_target;
+	conn_class->channel_get_args   = pk_connection_dbus_real_channel_get_args;
+	conn_class->channel_get_dir    = pk_connection_dbus_real_channel_get_dir;
+	conn_class->channel_get_env    = pk_connection_dbus_real_channel_get_env;
+	conn_class->channel_get_pid    = pk_connection_dbus_real_channel_get_pid;
+	conn_class->channel_get_state  = pk_connection_dbus_real_channel_get_state;
+}
+
+static void
+pk_connection_dbus_init (PkConnectionDBus *connection)
+{
+	connection->priv = G_TYPE_INSTANCE_GET_PRIVATE (connection,
+	                                                PK_TYPE_CONNECTION_DBUS,
+	                                                PkConnectionDBusPrivate);
+	g_static_rw_lock_init (&connection->priv->rw_lock);
+}
+
+PkConnection*
+pk_connection_dbus_new (void)
+{
+	return g_object_new (PK_TYPE_CONNECTION_DBUS, NULL);
 }
