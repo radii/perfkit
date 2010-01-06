12779bb36374b6057810533bc56aae241570fa90
Christian Hergert <chris@dronelabs.com>
2010-01-05 23:48:20 -0800

    [daemon] Source cleanup.

--- perfkit-daemon/sources/pkd-memory.c
+++ perfkit-daemon/sources/pkd-memory.c
@@ -61,19 +61,6 @@ struct _PkdMemoryPrivate
 	gboolean     running;
 };
 
-/**
- * pkd_memory_new:
- *
- * Creates a new instance of #PkdMemory.
- *
- * Return value: the newly created #PkdMemory instance.
- */
-PkdSource*
-pkd_memory_new (void)
-{
-	return g_object_new (PKD_TYPE_MEMORY, NULL);
-}
-
 typedef struct
 {
 	gint size;
@@ -84,10 +71,10 @@ typedef struct
 	gint data;
 	gint dt;
 } proc_pid_statm;
- 
-static gboolean
+
+static inline gboolean
 proc_pid_statm_read (proc_pid_statm *pstat,
-                     GPid pid)
+                     GPid            pid)
 {
 	gint fd;
 	gchar path[64];
@@ -99,12 +86,12 @@ proc_pid_statm_read (proc_pid_statm *pstat,
 	fd = open (path, O_RDONLY);
 	if (fd < 0)
 		return FALSE;
-	 
+
 	if (read (fd, buffer, sizeof (buffer) < 1)) {
 		close (fd);
 		return FALSE;
 	}
-	 
+ 
 	sscanf (buffer,
 	        "%d %d %d %d %d %d %d",
 	        &pstat->size,
@@ -148,35 +135,91 @@ thread_func (gpointer data)
 	pkd_manifest_append(m, "share", G_TYPE_UINT);
 	pkd_manifest_append(m, "text", G_TYPE_UINT);
 	pkd_manifest_append(m, "data", G_TYPE_UINT);
+
+	/*
+	 * Deliver the manifest to the subscription.
+	 */
 	pkd_source_deliver_manifest(data, m);
 
 	do {
+		/*
+		 * Generate the absolute timeout for our condition before we sample
+		 * so there is less sampling drift.
+		 */
 		g_get_current_time(&tv);
 		g_time_val_add_span(&tv, &span);
 
 		/*
-		 * Create our sample.
+		 * Parse /proc/pid/statm for memory statistics.
 		 */
 		if (proc_pid_statm_read(&st, pid)) {
+			/*
+			 * Generate sample.
+			 */
 			s = pkd_sample_new();
 			pkd_sample_append_uint(s, 1, st.size);
 			pkd_sample_append_uint(s, 2, st.resident);
 			pkd_sample_append_uint(s, 3, st.share);
 			pkd_sample_append_uint(s, 4, st.text);
 			pkd_sample_append_uint(s, 5, st.data);
+
+			/*
+			 * Deliver sample to subscription.
+			 */
 			pkd_source_deliver_sample(data, s);
+
+			/*
+			 * Done with the sample.
+			 */
+			pkd_sample_unref(s);
 		}
 
 		g_mutex_lock(priv->mutex);
+
+		/*
+		 * Check to see if we are done sampling.
+		 */
 		if (!(done = !priv->running))
 			goto unlock;
+
+		/*
+		 * Block until our timeout has passed or we have stopped.
+		 */
 		g_cond_timed_wait(priv->cond, priv->mutex, &tv);
+
+		/*
+		 * Check if we finished sampling while sleeping.
+		 */
 		done = !priv->running;
+
 unlock:
 		g_mutex_unlock(priv->mutex);
 	} while (!done);
 
+	/*
+	 * Finished with our manifest.
+	 */
+	pkd_manifest_unref(m);
+
 	return NULL;
+}
+
+static void
+pkd_memory_notify_stopped (PkdSource *source)
+{
+	PkdMemoryPrivate *priv;
+
+	priv = PKD_MEMORY(source)->priv;
+
+	g_mutex_lock(priv->mutex);
+
+	/*
+	 * Notify the thread we have completed.
+	 */
+	priv->running = FALSE;
+	g_cond_signal(priv->cond);
+
+	g_mutex_unlock(priv->mutex);
 }
 
 static void
@@ -190,7 +233,6 @@ pkd_memory_notify_start (PkdSource    *source,
 	priv->running = TRUE;
 
 	g_message("Starting memory thread.");
-
 	g_thread_create(thread_func, source, FALSE, NULL);
 }
 
@@ -202,6 +244,9 @@ pkd_memory_finalize (GObject *object)
 	g_return_if_fail (PKD_IS_MEMORY (object));
 
 	priv = PKD_MEMORY (object)->priv;
+
+	g_mutex_free(priv->mutex);
+	g_cond_free(priv->cond);
 
 	G_OBJECT_CLASS(pkd_memory_parent_class)->finalize(object);
 }
@@ -218,6 +263,7 @@ pkd_memory_class_init (PkdMemoryClass *klass)
 
 	source_class = PKD_SOURCE_CLASS(klass);
 	source_class->notify_started = pkd_memory_notify_start;
+	source_class->notify_stopped = pkd_memory_notify_stopped;
 }
 
 static void
@@ -229,4 +275,10 @@ pkd_memory_init (PkdMemory *memory)
 	memory->priv->span = G_TIME_SPAN_SECOND;
 	memory->priv->mutex = g_mutex_new();
 	memory->priv->cond = g_cond_new();
+}
+
+PkdSource*
+pkd_memory_new (void)
+{
+	return g_object_new(PKD_TYPE_MEMORY, NULL);
 }
