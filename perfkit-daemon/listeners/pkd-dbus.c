4271d52863506d426577c2c4f085951f1d3ffa3d
Christian Hergert <chris@dronelabs.com>
2010-03-31 03:02:56 -0700

    [daemon] Unregister source plugins on shutdown.

--- perfkit-daemon/listeners/pkd-dbus.c
+++ perfkit-daemon/listeners/pkd-dbus.c
@@ -145,6 +145,18 @@ pkd_dbus_start(PkdListener  *listener,
 static void
 pkd_dbus_stop(PkdListener *listener)
 {
+	GList *list, *iter;
+
+	g_message("Removing source plugins from DBus.");
+
+	list = pkd_pipeline_get_source_plugins();
+	for (iter = list; iter; iter = iter->next) {
+		dbus_g_connection_unregister_g_object(dbus_conn, iter->data);
+	}
+	g_list_foreach(list, (GFunc)g_object_unref, NULL);
+	g_list_free(list);
+
+	g_message("DBus listener stopped.");
 }
 
 static void
