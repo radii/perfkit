b9b91e77510ebfa25739c3632f2694097d720d24
Christian Hergert <chris@dronelabs.com>
2010-05-15 13:05:13 -0700

    [agent] Implement pka_channel_get_pid_set() RPC.

--- perfkit-agent/pka-listener.c
+++ perfkit-agent/pka-listener.c
@@ -575,25 +575,6 @@ pka_listener_channel_get_pid_finish (PkaListener    *listener, /* IN */
 	RETURN(ret);
 }
 
-#if 0
-static void
-pka_listener_channel_get_pid_set_cb (GObject      *listener,    /* IN */
-                                     GAsyncResult *result,      /* IN */
-                                     gpointer      user_data)   /* IN */
-{
-	GSimpleAsyncResult *real_result;
-
-	g_return_if_fail(PKA_IS_LISTENER(listener));
-	g_return_if_fail(RESULT_IS_VALID(channel_get_pid_set));
-
-	ENTRY;
-	real_result = GET_RESULT_POINTER(result);
-	g_simple_async_result_set_op_res_gpointer(real_result, result);
-	g_simple_async_result_complete(real_result);
-	EXIT;
-}
-#endif
-
 /**
  * pk_connection_channel_get_pid_set_async:
  * @connection: A #PkConnection.
@@ -617,6 +598,7 @@ pka_listener_channel_get_pid_set_async (PkaListener           *listener,    /* I
                                         GAsyncReadyCallback    callback,    /* IN */
                                         gpointer               user_data)   /* IN */
 {
+	ChannelGetPidSetCall *call;
 	GSimpleAsyncResult *result;
 
 	g_return_if_fail(PKA_IS_LISTENER(listener));
@@ -626,15 +608,11 @@ pka_listener_channel_get_pid_set_async (PkaListener           *listener,    /* I
 	                                   callback,
 	                                   user_data,
 	                                   pka_listener_channel_get_pid_set_async);
-// TEMP TO TEST RPC RESULTS
+	call = ChannelGetPidSetCall_Create();
+	g_simple_async_result_set_op_res_gpointer(
+			result, call, (GDestroyNotify)ChannelGetPidSetCall_Free);
 	g_simple_async_result_complete(result);
 	g_object_unref(result);
-#if 0
-	pka_channel_get_pid_set_async(instance,
-	                              NULL,
-	                              pka_listener_channel_get_pid_set_cb,
-	                              result);
-#endif
 	EXIT;
 }
 
@@ -658,23 +636,24 @@ pka_listener_channel_get_pid_set_finish (PkaListener    *listener, /* IN */
                                          gboolean       *pid_set,  /* OUT */
                                          GError        **error)    /* OUT */
 {
-	ENTRY;
-// TEMP TO TEST RPC RESULTS
-	RETURN(TRUE);
-#if 0
-	GSimpleAsyncResult *real_result;
-	gboolean ret;
+	PkaChannel *channel;
+	ChannelGetPidSetCall *call;
+	gboolean ret = FALSE;
 
 	g_return_val_if_fail(PKA_IS_LISTENER(listener), FALSE);
+	g_return_val_if_fail(RESULT_IS_VALID(channel_get_pid_set), FALSE);
 
 	ENTRY;
-	real_result = GET_RESULT_POINTER(result);
-	ret = pka_channel_get_pid_set_finish(instance,
-	                                     real_result,
-	                                     pid_set,
-	                                     error);
+	call = GET_RESULT_POINTER(ChannelGetPidSetCall, result);
+	if (!pka_manager_find_channel(DEFAULT_CONTEXT, call->channel,
+	                              &channel, error)) {
+		GOTO(failed);
+	}
+	*pid_set = pka_channel_get_pid_set(channel);
+	g_object_unref(channel);
+	ret = TRUE;
+  failed:
 	RETURN(ret);
-#endif
 }
 
 #if 0
