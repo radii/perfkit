35be15d61f7993387e46182f680a2bd9cee6232c
Christian Hergert <chris@dronelabs.com>
2010-05-16 02:00:15 -0700

    [libperfkit] Add PkManifest and PkSample back.

---
+++ perfkit/pk-manifest.c
@@ -0,0 +1,420 @@
+/* pk-manifest.c
+ *
+ * Copyright (C) 2010 Christian Hergert
+ *
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <egg-buffer.h>
+#include <egg-time.h>
+
+#include "pk-manifest.h"
+
+/**
+ * SECTION:pk-manifest
+ * @title: PkManifest
+ * @short_description: 
+ *
+ * 
+ */
+
+static gboolean decode (PkManifest *manifest, EggBuffer *buffer);
+
+struct _PkManifest
+{
+	volatile gint ref_count;
+
+	GTimeVal      tv;
+	PkResolution  resolution;
+	gint          source_id;
+	gint          n_rows;
+	GArray       *rows;
+};
+
+typedef struct
+{
+	gint   id;
+	GType  type;
+	gchar *name;
+} PkManifestRow;
+
+static void
+pk_manifest_destroy (PkManifest *manifest)
+{
+	gint i;
+
+	g_return_if_fail(manifest != NULL);
+
+	/* free row names */
+	for (i = 0; i < manifest->n_rows; i++) {
+		g_free(g_array_index(manifest->rows, PkManifestRow, i).name);
+	}
+
+	/* free row array */
+	g_array_unref(manifest->rows);
+}
+
+static PkManifest*
+pk_manifest_new (void)
+{
+	PkManifest *manifest;
+
+	manifest = g_slice_new0(PkManifest);
+	manifest->ref_count = 1;
+	manifest->rows = g_array_new(FALSE, FALSE, sizeof(PkManifestRow));
+
+	return manifest;
+}
+
+/**
+ * pk_manifest_new_from_data:
+ * @data: The manifest data.
+ * @length: The length of data.
+ *
+ * Creates a new instance of #PkManifest from a buffer of data.
+ *
+ * Returns: the newly created #PkManifest instance.
+ *
+ * Side effects: None.
+ */
+PkManifest*
+pk_manifest_new_from_data (const guint8 *data,
+                           gsize         length)
+{
+	PkManifest *manifest;
+	EggBuffer *buffer;
+
+	manifest = pk_manifest_new();
+	buffer = egg_buffer_new_from_data(data, length);
+
+	if (!decode(manifest, buffer)) {
+		goto error;
+	}
+
+	egg_buffer_unref(buffer);
+
+	return manifest;
+
+error:
+	egg_buffer_unref(buffer);
+	pk_manifest_unref(manifest);
+	return NULL;
+}
+
+/**
+ * pk_manifest_ref:
+ * manifest: A #PkManifest
+ *
+ * Atomically increments the reference count of @manifest by one.
+ *
+ * Returns: The @manifest pointer with its reference count incremented.
+ *
+ * Side effects: None.
+ */
+PkManifest*
+pk_manifest_ref (PkManifest *manifest)
+{
+	g_return_val_if_fail(manifest != NULL, NULL);
+	g_return_val_if_fail(manifest->ref_count > 0, NULL);
+
+	g_atomic_int_inc(&manifest->ref_count);
+
+	return manifest;
+}
+
+/**
+ * pk_manifest_unref:
+ * manifest: A #PkManifest
+ *
+ * Atomically decrements the reference count of @manifest by one.
+ * When the reference count reaches zero, the structures resources as well as
+ * the structure are freed.
+ *
+ * Returns: The @manifest pointer with its reference count incremented.
+ *
+ * Side effects: None.
+ */
+void
+pk_manifest_unref (PkManifest *manifest)
+{
+	g_return_if_fail(manifest != NULL);
+	g_return_if_fail(manifest->ref_count > 0);
+
+	if (g_atomic_int_dec_and_test(&manifest->ref_count)) {
+		pk_manifest_destroy(manifest);
+		g_slice_free(PkManifest, manifest);
+	}
+}
+
+/**
+ * pk_manifest_get_resolution:
+ * @manifest: A #PkManifest.
+ *
+ * Retrieves the time resolution of the manifest here-forth.  This can be
+ * used for bucketing of samples.
+ *
+ * Returns: The #PkResolution of the manifest.
+ *
+ * Side effects: None.
+ */
+PkResolution
+pk_manifest_get_resolution (PkManifest *manifest)
+{
+	g_return_val_if_fail(manifest != NULL, 0);
+
+	return manifest->resolution;
+}
+
+/**
+ * pk_manifest_get_n_rows:
+ * @manifest: A #PkManifest.
+ *
+ * Retrieves the number of rows in the manifest.
+ *
+ * Returns: The number of rows.
+ *
+ * Side effects: None.
+ */
+gint
+pk_manifest_get_n_rows (PkManifest *manifest)
+{
+	g_return_val_if_fail(manifest != NULL, 0);
+
+	return manifest->n_rows;
+}
+
+/**
+ * pk_manifest_get_row_type:
+ * @manifest: A #PkManifest.
+ * @row: row number which starts from 1.
+ *
+ * Retrieves the #GType for the row content.
+ *
+ * Returns: A #GType.
+ *
+ * Side effects: None.
+ */
+GType
+pk_manifest_get_row_type (PkManifest *manifest,
+                          gint        row)
+{
+	g_return_val_if_fail(manifest != NULL, G_TYPE_INVALID);
+	g_return_val_if_fail(row > 0 && row <= manifest->n_rows, G_TYPE_INVALID);
+
+	return g_array_index(manifest->rows, PkManifestRow, row - 1).type;
+}
+
+/**
+ * pk_manifest_get_row_name:
+ * @manifest: A #PkManifest.
+ * @row: row number which starts from 1.
+ *
+ * Retrieves the name of a row.
+ *
+ * Returns: 
+ *
+ * Side effects: None.
+ */
+const gchar*
+pk_manifest_get_row_name (PkManifest *manifest,
+                          gint        row)
+{
+	g_return_val_if_fail(manifest != NULL, NULL);
+	g_return_val_if_fail(row > 0 && row <= manifest->n_rows, NULL);
+
+	return g_array_index(manifest->rows, PkManifestRow, row - 1).name;
+}
+
+/**
+ * pk_manifest_get_timeval:
+ * @manifest: A #PkManifest.
+ * @row: row number which starts from 1.
+ *
+ * Retrieves the #GTimeVal for with the manifest is authoritative.
+ *
+ * Side effects: None.
+ */
+void
+pk_manifest_get_timeval (PkManifest *manifest,
+                         GTimeVal   *tv)
+{
+	g_return_if_fail(manifest != NULL);
+	g_return_if_fail(tv != NULL);
+
+	*tv = manifest->tv;
+}
+
+GType
+pk_manifest_get_type (void)
+{
+	static GType type_id = 0;
+	GType _type_id;
+
+	if (g_once_init_enter((gsize *)&type_id)) {
+		_type_id = g_boxed_type_register_static("PkManifest",
+		                                        (GBoxedCopyFunc)pk_manifest_ref,
+		                                        (GBoxedFreeFunc)pk_manifest_unref);
+		g_once_init_leave((gsize *)&type_id, _type_id);
+	}
+
+	return type_id;
+}
+
+static gint
+sort_func (gconstpointer a,
+           gconstpointer b)
+{
+	const PkManifestRow *row_a = a,
+	                    *row_b = b;
+
+	return (row_a->id == row_b->id) ? 0 : row_a->id - row_b->id;
+}
+
+static gboolean
+decode (PkManifest *manifest,
+        EggBuffer  *buffer)
+{
+	guint field, tag, u32, len;
+	guint64 u64;
+	gsize end;
+	gint i;
+
+	g_return_val_if_fail(manifest != NULL, FALSE);
+	g_return_val_if_fail(buffer != NULL, FALSE);
+
+	/* timestamp */
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+	if (tag != EGG_BUFFER_UINT64 || field != 1) {
+		return FALSE;
+	}
+	if (!egg_buffer_read_uint64(buffer, &u64)) {
+		return FALSE;
+	}
+	g_time_val_from_ticks(&manifest->tv, (GTimeSpan *)&u64);
+
+	/* resolution */
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+	if (tag != EGG_BUFFER_UINT || field != 2) {
+		return FALSE;
+	}
+	if (!egg_buffer_read_uint(buffer, &u32)) {
+		return FALSE;
+	}
+	if (u32 > PK_RESOLUTION_HOUR) {
+		return FALSE;
+	}
+	manifest->resolution = u32;
+
+	/* source */
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+	if (tag != EGG_BUFFER_UINT || field != 3) {
+		return FALSE;
+	}
+	if (!egg_buffer_read_uint(buffer, &u32)) {
+		return FALSE;
+	}
+	manifest->source_id = u32;
+
+	/* columns */
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+	if (tag != EGG_BUFFER_REPEATED || field != 4) {
+		return FALSE;
+	}
+
+	/* len of data */
+	if (!egg_buffer_read_uint(buffer, &len)) {
+		return FALSE;
+	}
+
+	/* determine end of buffer */
+	end = egg_buffer_get_pos(buffer) + len;
+
+	/* get manifest rows */
+	while (egg_buffer_get_pos(buffer) < end) {
+		PkManifestRow row;
+		gchar *name = NULL;
+		guint row_id = 0;
+		GType row_type = 0;
+
+		/* row data length */
+		if (!egg_buffer_read_uint(buffer, &u32)) {
+			return FALSE;
+		}
+
+		/* row id */
+		if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+			return FALSE;
+		}
+		if (field != 1 || tag != EGG_BUFFER_UINT) {
+			return FALSE;
+		}
+		if (!egg_buffer_read_uint(buffer, &row_id)) {
+			return FALSE;
+		}
+
+		/* row type */
+		if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+			return FALSE;
+		}
+		if (field != 2 || tag != EGG_BUFFER_ENUM) {
+			return FALSE;
+		}
+		if (!egg_buffer_read_uint(buffer, (guint *)&row_type)) {
+			return FALSE;
+		}
+
+		/* row name */
+		if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+			return FALSE;
+		}
+		if (field != 3 || tag != EGG_BUFFER_STRING) {
+			return FALSE;
+		}
+		if (!egg_buffer_read_string(buffer, &name)) {
+			return FALSE;
+		}
+
+		row.id = row_id;
+		row.type = row_type;
+		row.name = name;
+
+		g_array_append_val(manifest->rows, row);
+		g_array_sort(manifest->rows, sort_func);
+		manifest->n_rows++;
+	}
+
+	/* make sure all rows were sent */
+	for (i = 0; i < manifest->n_rows; i++) {
+		PkManifestRow *row;
+
+		row = &g_array_index(manifest->rows, PkManifestRow, i);
+		if (row->id != (i + 1)) {
+			return FALSE;
+		}
+	}
+
+	return TRUE;
+}
---
+++ perfkit/pk-sample.c
@@ -0,0 +1,468 @@
+/* pk-sample.c
+ * 
+ * Copyright (C) 2009 Christian Hergert
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <egg-buffer.h>
+#include <egg-time.h>
+
+#include "pk-manifest.h"
+#include "pk-sample.h"
+
+/**
+ * SECTION:pk-sample
+ * @title: PkSample
+ * @short_description: 
+ *
+ * 
+ */
+
+typedef struct _PkSampleField PkSampleField;
+
+struct _PkSample
+{
+	volatile gint ref_count;
+
+	GTimeVal  tv;
+	GArray   *ar;
+};
+
+struct _PkSampleField
+{
+	guint  field;
+	GValue value;
+};
+
+static void
+pk_sample_destroy (PkSample *sample)
+{
+	gint i;
+
+	g_return_if_fail(sample != NULL);
+
+	if (G_LIKELY(sample->ar)) {
+		for (i = 0; i < sample->ar->len; i++) {
+			g_value_unset(&(g_array_index(sample->ar, PkSampleField, i).value));
+		}
+	}
+}
+
+/**
+ * pk_sample_new:
+ *
+ * Creates a new instance of #PkSample.
+ *
+ * Returns: The newly created instance of PkSample.
+ *
+ * Side effects: None.
+ */
+static PkSample*
+pk_sample_new (void)
+{
+	PkSample *sample;
+
+	sample = g_slice_new0(PkSample);
+	sample->ref_count = 1;
+	sample->ar = g_array_new(FALSE, FALSE, sizeof(PkSampleField));
+
+	return sample;
+}
+
+static gboolean
+pk_sample_decode_timeval (PkSample   *sample,
+                          EggBuffer  *buffer,
+                          PkManifest *manifest)
+{
+	PkResolution res;
+	guint field, tag;
+	GTimeVal tv;
+	guint64 u64;
+
+	/*
+	 * Make sure the buffer is at the timestamp field.
+	 */
+
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+
+	if (field != 1 || tag != EGG_BUFFER_UINT64) {
+		return FALSE;
+	}
+
+	if (!egg_buffer_read_uint64(buffer, &u64)) {
+		return FALSE;
+	}
+
+	/*
+	 * Resolve the relative timestamp.
+	 */
+
+	pk_manifest_get_timeval(manifest, &tv);
+	res = pk_manifest_get_resolution(manifest);
+
+	switch (res) {
+	case PK_RESOLUTION_PRECISE:
+	case PK_RESOLUTION_USEC:
+		tv.tv_sec += (u64 / G_TIME_SPAN_SECOND);
+		tv.tv_usec += (u64 % G_TIME_SPAN_SECOND) / 10;
+		break;
+	case PK_RESOLUTION_MSEC:
+		tv.tv_sec += (u64 / G_TIME_SPAN_SECOND);
+		tv.tv_usec += (u64 % G_TIME_SPAN_SECOND) / G_TIME_SPAN_MILLISECOND;
+		break;
+	case PK_RESOLUTION_SECOND:
+		tv.tv_sec += (u64 / G_TIME_SPAN_SECOND);
+		break;
+	case PK_RESOLUTION_MINUTE:
+		tv.tv_sec += (u64 / G_TIME_SPAN_MINUTE) * 60;
+		break;
+	case PK_RESOLUTION_HOUR:
+		tv.tv_sec += (u64 / G_TIME_SPAN_HOUR) * 3600;
+		break;
+	default:
+		g_warn_if_reached();
+		return FALSE;
+	}
+
+	sample->tv = tv;
+
+	return TRUE;
+}
+
+static inline gboolean
+pk_sample_init_value (PkSample   *sample,
+                      PkManifest *manifest,
+                      guint       field,
+                      GValue     *value)
+{
+	GType type_id;
+
+	type_id = pk_manifest_get_row_type(manifest, field);
+	if (type_id == G_TYPE_INVALID) {
+		return FALSE;
+	}
+
+	g_value_init(value, type_id);
+	return TRUE;
+}
+
+static gboolean
+pk_sample_decode_data (PkSample   *sample,
+                       EggBuffer  *buffer,
+                       PkManifest *manifest)
+{
+	guint field, tag;
+	guint data_len;
+	gsize total_len,
+	      offset;
+
+	/* Make sure the buffer is at field 2, data. */
+	if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+		return FALSE;
+	}
+
+	if (field != 2 || tag != EGG_BUFFER_DATA) {
+		return FALSE;
+	}
+
+	/* Get the length of the data section. */
+	if (!egg_buffer_read_uint(buffer, &data_len)) {
+		return FALSE;
+	}
+
+	/* Make sure there is enough data in the buffer. */
+	total_len = egg_buffer_get_length(buffer);
+	offset = egg_buffer_get_pos(buffer);
+	if (total_len < (offset + data_len)) {
+		return FALSE;
+	}
+
+	while (offset < total_len) {
+		PkSampleField item;
+		GValue value = {0};
+		GType type;
+		gchar *str;
+
+		/* Get the field position and type. */
+		if (!egg_buffer_read_tag(buffer, &field, &tag)) {
+			return FALSE;
+		}
+
+		/* Initialize the gvalue type from the manifest */
+		if (!pk_sample_init_value(sample, manifest, field, &value)) {
+			return FALSE;
+		}
+
+		/* Get the type for verification */
+		type = G_VALUE_TYPE(&value);
+
+		/* Extract field data */
+		switch (tag) {
+		case 0: /* Varint type */
+			switch (type) {
+			case G_TYPE_INT:
+				if (!egg_buffer_read_int(buffer, &value.data[0].v_int)) {
+					return FALSE;
+				}
+				break;
+			case G_TYPE_UINT:
+				if (!egg_buffer_read_uint(buffer, &value.data[0].v_uint)) {
+					return FALSE;
+				}
+				break;
+			case G_TYPE_INT64:
+				if (!egg_buffer_read_int64(buffer, &value.data[0].v_int64)) {
+					return FALSE;
+				}
+				break;
+			case G_TYPE_UINT64:
+				if (!egg_buffer_read_uint64(buffer, &value.data[0].v_uint64)) {
+					return FALSE;
+				}
+				break;
+			default:
+				return FALSE;
+			}
+			break;
+
+		case 1: /* Double type */
+			switch (type) {
+			case G_TYPE_DOUBLE:
+				if (!egg_buffer_read_double(buffer, &value.data[0].v_double)) {
+					return FALSE;
+				}
+				break;
+			default:
+				return FALSE;
+			}
+			break;
+
+		case 2: /* String/data type */
+			switch (type) {
+			case G_TYPE_STRING:
+				if (!egg_buffer_read_string(buffer, &str)) {
+					return FALSE;
+				}
+				g_value_take_string(&value, str);
+				break;
+			default:
+				return FALSE;
+			}
+			break;
+
+		case 5: /* Float type */
+			switch (type) {
+			case G_TYPE_FLOAT:
+				if (!egg_buffer_read_float(buffer, &value.data[0].v_float)) {
+					return FALSE;
+				}
+				break;
+			default:
+				return FALSE;
+			}
+			break;
+
+		default: /* Invalid type */
+			return FALSE;
+		}
+
+		/* Add sample data point */
+		item.field = field;
+		item.value = value;
+		g_array_append_val(sample->ar, item);
+
+		/* Increment buffer position */
+		offset = egg_buffer_get_pos(buffer);
+	}
+
+	return TRUE;
+}
+
+/**
+ * pk_sample_decode:
+ * @sample: A #PkSample.
+ * @buffer: An #EggBuffer.
+ * @manifest: A #PkManifest.
+ *
+ * Decodes the sample data within the buffer.
+ *
+ * Retruns: %TRUE if successful; otherwise %FALSE.
+ *
+ * Side effects: None.
+ */
+static gboolean
+pk_sample_decode (PkSample   *sample,
+                  EggBuffer  *buffer,
+                  PkManifest *manifest)
+{
+	g_return_val_if_fail(sample != NULL, FALSE);
+	g_return_val_if_fail(buffer != NULL, FALSE);
+
+	/* Decode field 1, timestamp. */
+	if (!pk_sample_decode_timeval(sample, buffer, manifest)) {
+		return FALSE;
+	}
+
+	/* Decode field 2, data. */
+	if (!pk_sample_decode_data(sample, buffer, manifest)) {
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+/**
+ * pk_sample_new_from_data:
+ * @data: a buffer of data.
+ * @length: the length of the buffer.
+ *
+ * Creates a new #PkSample from a buffer of data.
+ *
+ * Returns: the #PkSample if successful; otherwise NULL.
+ *
+ * Side effects: None.
+ */
+PkSample*
+pk_sample_new_from_data (PkManifest   *manifest,
+                         const guint8 *data,
+                         gsize         length,
+                         gsize        *n_read)
+{
+	PkSample *sample;
+	EggBuffer *buffer;
+
+	g_return_val_if_fail(manifest != NULL, NULL);
+	g_return_val_if_fail(data != NULL, NULL);
+	g_return_val_if_fail(n_read != NULL, NULL);
+
+	sample = pk_sample_new();
+	buffer = egg_buffer_new_from_data(data, length);
+
+	if (!pk_sample_decode(sample, buffer, manifest)) {
+		pk_sample_unref(sample);
+		sample = NULL;
+	}
+
+	*n_read = egg_buffer_get_pos(buffer);
+	egg_buffer_unref(buffer);
+
+	return sample;
+}
+
+/**
+ * pk_sample_ref:
+ * sample: A #PkSample
+ *
+ * Atomically increments the reference count of @sample by one.
+ *
+ * Returns: The @sample pointer with its reference count incremented.
+ *
+ * Side effects: None.
+ */
+PkSample*
+pk_sample_ref (PkSample *sample)
+{
+	g_return_val_if_fail(sample != NULL, NULL);
+	g_return_val_if_fail(sample->ref_count > 0, NULL);
+
+	g_atomic_int_inc(&sample->ref_count);
+
+	return sample;
+}
+
+/**
+ * pk_sample_unref:
+ * sample: A #PkSample
+ *
+ * Atomically decrements the reference count of @sample by one.
+ * When the reference count reaches zero, the structures resources as well as
+ * the structure are freed.
+ *
+ * Returns: The @sample pointer with its reference count incremented.
+ *
+ * Side effects: None.
+ */
+void
+pk_sample_unref (PkSample *sample)
+{
+	g_return_if_fail(sample != NULL);
+	g_return_if_fail(sample->ref_count > 0);
+
+	if (g_atomic_int_dec_and_test(&sample->ref_count)) {
+		pk_sample_destroy(sample);
+		g_slice_free(PkSample, sample);
+	}
+}
+
+/**
+ * pk_sample_get_value:
+ * @sample: A #PkSample
+ * @row_id: The row within the manifest.
+ * @value: A #GValue to initialize and set.
+ *
+ * Retrieves the value for a given row in the sample.
+ *
+ * Returns: %TRUE if successful; otherwise %FALSE.
+ */
+gboolean
+pk_sample_get_value (PkSample *sample,  // IN
+                     guint     row_id,  // IN
+                     GValue   *value)   // OUT
+{
+	PkSampleField *f;
+	gint i;
+
+	g_return_val_if_fail(sample != NULL, FALSE);
+	g_return_val_if_fail(value != NULL, FALSE);
+	g_return_val_if_fail(sample->ar != NULL, FALSE);
+
+	for (i = 0; i < sample->ar->len; i++) {
+		f = &g_array_index(sample->ar, PkSampleField, i);
+		if (f->field == row_id) {
+			g_value_init(value, G_VALUE_TYPE(&f->value));
+			g_value_copy(&f->value, value);
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+/**
+ * pk_sample_get_type:
+ *
+ * Returns: The #PkSample #GType.
+ */
+GType
+pk_sample_get_type (void)
+{
+	static GType type_id = 0;
+	GType _type_id;
+
+	if (g_once_init_enter((gsize *)&type_id)) {
+		_type_id = g_boxed_type_register_static("PkSample",
+		                                        (GBoxedCopyFunc)pk_sample_ref,
+		                                        (GBoxedFreeFunc)pk_sample_unref);
+		g_once_init_leave((gsize *)&type_id, _type_id);
+	}
+
+	return type_id;
+}
