25307a9c145e7f02df6ec61dc0e6be6f123db8fb
Christian Hergert <chris@dronelabs.com>
2010-05-28 23:37:45 -0700

    [build] Use PERFKIT_CONNECTIONS_DIR.

--- tests/perfkit/test-pk-connection.c
+++ tests/perfkit/test-pk-connection.c
@@ -14,7 +14,7 @@ gint
 main (gint   argc,
       gchar *argv[])
 {
-	g_setenv("PK_CONNECTIONS_DIR", PK_CONNECTIONS_DIR, FALSE);
+	g_setenv("PERFKIT_CONNECTIONS_DIR", PERFKIT_CONNECTIONS_DIR, FALSE);
 
 	g_type_init();
 	g_test_init(&argc, &argv, NULL);
