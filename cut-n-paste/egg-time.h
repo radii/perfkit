cbaa8bdc15478bbe242815269d3c9b3ad549db3d
Christian Hergert <chris@dronelabs.com>
2010-06-06 20:43:51 -0700

    [cut-n-paste] Add new egg-time with timespec routines.

--- cut-n-paste/egg-time.h
+++ cut-n-paste/egg-time.h
@@ -1,24 +1,19 @@
 /* egg-time.h
  *
- * Copyright (c) 2010 Christian Hergert
+ * Copyright (C) 2010 Christian Hergert <chris@dronelabs.com>
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * This file is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef __EGG_TIME_H__
@@ -27,70 +22,88 @@
 #include <glib.h>
 #include <time.h>
 
-G_BEGIN_DECLS
+#ifndef G_NSEC_PER_SEC
+#define G_NSEC_PER_SEC 1000000000
+#endif
 
-#define G_NSEC_PER_SEC          (1000000000)
-#define G_TIME_SPAN_DAY         (G_GINT64_CONSTANT (864000000000))
-#define G_TIME_SPAN_HOUR        (G_GINT64_CONSTANT (36000000000))
-#define G_TIME_SPAN_MINUTE      (G_GINT64_CONSTANT (600000000))
-#define G_TIME_SPAN_SECOND      (G_GINT64_CONSTANT (10000000))
-#define G_TIME_SPAN_MILLISECOND (G_GINT64_CONSTANT (10000))
-
-typedef gint64 GTimeSpan;
-
+/**
+ * timespec_subtract:
+ * @x: A struct timespec.
+ *
+ * Subtracts @y from @x and stores the result in @z.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static inline void
-g_time_val_to_ticks (GTimeVal  *tv,
-                     GTimeSpan *u)
+timespec_subtract (struct timespec *x, /* IN */
+                   struct timespec *y, /* IN */
+                   struct timespec *z) /* OUT */
 {
-	*u = ((((guint64)tv->tv_sec) + G_GINT64_CONSTANT(62135596800)) * 10000000)
-	   + (((guint64)tv->tv_usec) * 10);
+	z->tv_sec = (x->tv_sec - y->tv_sec);
+	if ((z->tv_nsec = (x->tv_nsec - y->tv_nsec)) < 0) {
+		z->tv_nsec += G_NSEC_PER_SEC;
+		z->tv_sec -= 1;
+	}
 }
 
+/**
+ * timespec_add:
+ * @x: A struct timespec to add.
+ * @y: A struct timespec to add.
+ * @z: A struct timespec to store the result.
+ *
+ * Adds @x and @y together and stores the result in @z.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static inline void
-g_time_val_from_ticks (GTimeVal  *tv,
-                       GTimeSpan *u)
+timespec_add (struct timespec *x, /* IN */
+              struct timespec *y, /* IN */
+              struct timespec *z) /* OUT */
 {
-	tv->tv_usec = ((*u) / 10) % G_TIME_SPAN_SECOND;
-	tv->tv_sec = ((*u) / G_TIME_SPAN_SECOND) - G_GINT64_CONSTANT(62135596800);
+	z->tv_sec = x->tv_sec + y->tv_sec;
+	if ((z->tv_nsec = (x->tv_nsec + y->tv_nsec)) > G_NSEC_PER_SEC) {
+		z->tv_nsec -= G_NSEC_PER_SEC;
+		z->tv_sec += 1;
+	}
 }
 
+/**
+ * timespec_to_usec:
+ * @t: A struct timespec.
+ * @u: A location for the uint64 encoding.
+ *
+ * Encodes @t in microseconds since the Epoch and stores the result in @u.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static inline void
-g_time_val_add_span (GTimeVal  *tv,
-                     GTimeSpan *ts)
+timespec_to_usec (struct timespec *t, /* IN */
+                  guint64         *u) /* IN */
 {
-	GTimeSpan u1;
-
-	g_time_val_to_ticks(tv, &u1);
-	u1 += *ts;
-	g_time_val_from_ticks(tv, &u1);
+	*u = (t->tv_sec * (guint64)G_USEC_PER_SEC);
+	*u += (t->tv_nsec / G_GUINT64_CONSTANT(1000));
 }
 
+/**
+ * timespec_from_usec:
+ * @t: A struct timespec.
+ * @u: Microseconds since the Epoch.
+ *
+ * Decodes u back into a struct timespec and stores it in @t.
+ *
+ * Returns: None.
+ * Side effects: None.
+ */
 static inline void
-g_time_val_diff (GTimeVal  *tv1,
-                 GTimeVal  *tv2,
-                 GTimeSpan *u)
+timespec_from_usec (struct timespec *t, /* OUT */
+                    guint64          u) /* IN */
 {
-	gint64 u1, u2;
-
-	g_time_val_to_ticks(tv1, &u1);
-	g_time_val_to_ticks(tv2, &u2);
-	*u = (u1 - u2);
+	t->tv_sec = u / (guint64)G_USEC_PER_SEC;
+	t->tv_nsec = (u % (guint64)G_USEC_PER_SEC) * G_GUINT64_CONSTANT(1000);
 }
-
-static inline gint
-g_time_val_compare (const GTimeVal *tv1,
-                    const GTimeVal *tv2)
-{
-	if ((tv1->tv_sec == tv2->tv_sec) && (tv1->tv_usec == tv2->tv_usec))
-		return 0;
-	else if ((tv1->tv_sec > tv2->tv_sec) ||
-	    ((tv1->tv_sec == tv2->tv_sec) &&
-	     (tv1->tv_usec > tv2->tv_usec)))
-		return 1;
-	else
-		return -1;
-}
-
-G_END_DECLS
 
 #endif /* __EGG_TIME_H__ */
