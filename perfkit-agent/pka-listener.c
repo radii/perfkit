3742d509af33d00f22f459aa8e1d68a3156e73b6
Christian Hergert <chris@dronelabs.com>
2010-06-19 16:47:04 -0700

    [agent] Implement Subscription::AddSource() RPC.

--- perfkit-agent/pka-listener.c
+++ perfkit-agent/pka-listener.c
@@ -3606,25 +3606,6 @@ pka_listener_subscription_add_channel_finish (PkaListener    *listener, /* IN */
 	RETURN(ret);
 }
 
-#if 0
-static void
-pka_listener_subscription_add_source_cb (GObject      *listener,    /* IN */
-                                         GAsyncResult *result,      /* IN */
-                                         gpointer      user_data)   /* IN */
-{
-	GSimpleAsyncResult *real_result;
-
-	g_return_if_fail(PKA_IS_LISTENER(listener));
-	g_return_if_fail(RESULT_IS_VALID(subscription_add_source));
-
-	ENTRY;
-	real_result = GET_RESULT_POINTER(result);
-	g_simple_async_result_set_op_res_gpointer(real_result, result);
-	g_simple_async_result_complete(real_result);
-	EXIT;
-}
-#endif
-
 /**
  * pk_connection_subscription_add_source_async:
  * @connection: A #PkConnection.
@@ -3651,6 +3632,7 @@ pka_listener_subscription_add_source_async (PkaListener           *listener,    
                                             GAsyncReadyCallback    callback,     /* IN */
                                             gpointer               user_data)    /* IN */
 {
+	SubscriptionAddSourceCall *call;
 	GSimpleAsyncResult *result;
 
 	g_return_if_fail(PKA_IS_LISTENER(listener));
@@ -3660,15 +3642,13 @@ pka_listener_subscription_add_source_async (PkaListener           *listener,    
 	                                   callback,
 	                                   user_data,
 	                                   pka_listener_subscription_add_source_async);
-// TEMP TO TEST RPC RESULTS
+	call = SubscriptionAddSourceCall_Create();
+	call->subscription = subscription;
+	call->source = source;
+	g_simple_async_result_set_op_res_gpointer(
+			result, call, (GDestroyNotify)SubscriptionAddSourceCall_Free);
 	g_simple_async_result_complete(result);
 	g_object_unref(result);
-#if 0
-	pka_subscription_add_source_async(instance,
-	                                  NULL,
-	                                  pka_listener_subscription_add_source_cb,
-	                                  result);
-#endif
 	EXIT;
 }
 
@@ -3691,22 +3671,31 @@ pka_listener_subscription_add_source_finish (PkaListener    *listener, /* IN */
                                              GAsyncResult   *result,   /* IN */
                                              GError        **error)    /* OUT */
 {
-	ENTRY;
-// TEMP TO TEST RPC RESULTS
-	RETURN(TRUE);
-#if 0
-	GSimpleAsyncResult *real_result;
-	gboolean ret;
+	SubscriptionAddSourceCall *call;
+	PkaSubscription *subscription;
+	PkaSource *source;
+	gboolean ret = FALSE;
 
 	g_return_val_if_fail(PKA_IS_LISTENER(listener), FALSE);
+	g_return_val_if_fail(RESULT_IS_VALID(subscription_add_source), FALSE);
 
 	ENTRY;
-	real_result = GET_RESULT_POINTER(result);
-	ret = pka_subscription_add_source_finish(instance,
-	                                         real_result,
-	                                         error);
+	call = GET_RESULT_POINTER(SubscriptionAddSourceCall, result);
+	if (!pka_manager_find_subscription(DEFAULT_CONTEXT, call->subscription,
+	                                   &subscription, error)) {
+		GOTO(failed);
+	}
+	if (!pka_manager_find_source(DEFAULT_CONTEXT, call->source,
+	                             &source, error)) {
+	    pka_subscription_unref(subscription);
+		GOTO(failed);
+	}
+	ret = pka_subscription_add_source(subscription, DEFAULT_CONTEXT,
+	                                  source, error);
+	pka_subscription_unref(subscription);
+	g_object_unref(source);
+  failed:
 	RETURN(ret);
-#endif
 }
 
 /**
